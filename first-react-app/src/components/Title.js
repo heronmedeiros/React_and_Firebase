6af06e7a4bb7a6e2c9c9cf48e3038eae6cdf475f
Heron Medeiros <heron.medeiros@gmail.com>
2023-01-05 15:12:42 -0300

    extract specific style

--- first-react-app/src/components/Title.js
+++ first-react-app/src/components/Title.js
@@ -1,10 +1,11 @@
+import './Title.css'
 export default function Title({ title, subtitle }) {
     return(
         // <> -> fragment
-        <>
+        <div className='title-block'>
             <h1 className="title">{ title }</h1>
             <br />
             <h2 className="subtitle">{ subtitle }</h2>
-        </>
+        </div>
     )
 }
