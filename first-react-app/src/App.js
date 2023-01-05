b57942284b5e72ba203e8ca3888bcdd825bd824a
Heron Medeiros <heron.medeiros@gmail.com>
2023-01-05 11:27:01 -0300

    show and hide events

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -3,16 +3,41 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [ name, setName ] = useState('mario');
+  const [showEvents, setShowEvents] = useState(true);
 
-  const handleClick = () => {
-    setName('luigi');
-    console.log(name);
+  const [events, setEvents] = useState([
+    {title: 'heron birthday', id: 1},
+    {title: 'stream', id: 2},
+    {title: 'race', id: 3}
+  ])
+
+  const handleClick = (id) => {
+    setEvents((prevEvents) => {
+      return setEvents(events.filter((event) => {
+        return id !== event.id
+      }));
+    })
   }
+
   return (
     <div className="App">
-      <h1>My name is {name}</h1>
-      <button onClick={handleClick}>change name</button>
+      {showEvents && (
+        <div>
+          <button onClick={() => { setShowEvents(false) }}> hide events</button>
+        </div>
+      )}
+      {!showEvents && (
+        <div>
+          <button onClick={() => { setShowEvents(true) }}> show events</button>
+        </div>
+      )}
+
+      {showEvents && events.map( (event, index) => (
+        <div key={event.id}>
+          <h2>{index} - {event.title}</h2>
+          <button onClick={() => handleClick(event.id)}>delete event</button>
+        </div>
+      ))}
     </div>
   );
 }
