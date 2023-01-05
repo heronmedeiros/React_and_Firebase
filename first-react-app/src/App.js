0c3635962ac4ac305f4885635853acb1cf1a65c7
Heron Medeiros <heron.medeiros@gmail.com>
2023-01-05 10:50:25 -0300

    start using state

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [ name, setName ] = useState('mario');
+
+  const handleClick = () => {
+    setName('luigi');
+    console.log(name);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React & Firebase
-        </a>
-      </header>
+      <h1>My name is {name}</h1>
+      <button onClick={handleClick}>change name</button>
     </div>
   );
 }
